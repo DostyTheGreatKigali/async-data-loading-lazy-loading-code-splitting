085d713ab95d714e73e5453cd55227770cf85fa0
DostyTheGreatKigali <abumucal89@gmail.com>
2022-01-01 11:44:33 +0000

    Asynchronous part completed

---
+++ src/App_B4_ADDING_STATE.js
@@ -0,0 +1,13 @@
+import './App.css';
+import RiverInformation from './components/RiverInformation/RiverInformation';
+
+function App() {
+  return (
+    <div className="wrapper">
+    <h1>World's Longest Rivers</h1>
+    <RiverInformation />
+  </div>
+  );
+}
+
+export default App;
---
+++ src/components/RiverInformation/RiverInformation_BASIC_TEMPLATE.js
@@ -0,0 +1,10 @@
+// https://www.digitalocean.com/community/tutorials/how-to-handle-async-data-loading-lazy-loading-and-code-splitting-with-react
+import React from 'react';
+
+export default function RiverInformation() {
+  return(
+    <div>
+      <h2>River Information</h2>
+    </div>
+  )
+}
